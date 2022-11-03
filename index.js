985e1ef8150664604fa674220fadbf8dfc3509cf
jonathan gartland <jonny@hal.local>
2022-11-03 16:25:48 -0400

    practice, add a few more simple data types

--- index.js
+++ index.js
@@ -1,32 +1,72 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const data = { 
-  "temps": [
+  temps: [
     {
-      "id": 1,
-},
-{
-  "id": 2,
-}
+      id: 1,
+    },
+    {
+      id: 2,
+    }
   ]
 };
 
-const typeDefs = gql`
-  type temp {
-  id: ID
+const profileData = {
+  profiles: [ 
+    {
+      id: 1,
+      name: 'profile1',
+    },
+    {
+      id: 2,
+      name: 'profile2',
+    }
+  ]
+};
+
+const typesData = {
+  types: [
+    {
+      id: 1,
+      name: "type1",
+      typ: "a",
+    }
+  ]
+
 }
 
-type Query {
-  temps: [temp]
-}
+const typeDefs = gql`
+  type type {
+    id: ID,
+    name: String,
+    typ: String,
+  }
+
+  type profile {
+    id: ID,
+    name: String,
+  }
+
+  type temp {
+    id: ID
+  }
+
+  type Query {
+    temps: [temp],
+    profiles: [profile],
+    types:[type],
+  }
 `;
 
 const resolvers = {
   Query: {
-  temps: () => data.temps
-}
+  temps: () => data.temps,
+  profiles: () => profileData.profiles,
+  types: () => typesData.types
+  }
+
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen(24000);
+server.listen(26000);
